e8d94212d75738f171d30b30e21e91e22e2e1038
= <=>
2023-08-12 13:13:39 +0530

    add setMonthData mutation

--- src/server/api/routers/monthRouter.ts
+++ src/server/api/routers/monthRouter.ts
@@ -1,12 +1,15 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 
+const monthInput = z.object({ month: z.number(), year: z.number() });
+
 export const monthRouter = createTRPCRouter({
   getMonthData: protectedProcedure
-    .input(z.object({ month: z.number(), year: z.number() }))
+    .input(monthInput)
     .query(async ({ ctx, input }) => {
       const month = await ctx.prisma.month.findFirst({
         where: {
@@ -15,5 +18,30 @@ export const monthRouter = createTRPCRouter({
         },
       });
       return { month };
+    }),
+  setMonthData: protectedProcedure
+    .input(monthInput.extend({ budget: z.number() }))
+    .mutation(async ({ ctx, input: { budget, month, year } }) => {
+      const foundMonth = await ctx.prisma.month.findFirst({
+        where: {
+          year,
+          month,
+        },
+      });
+      if (foundMonth !== null) {
+        throw new TRPCError({
+          code: "CONFLICT",
+          message: "Data for the month already exists",
+        });
+      }
+      await ctx.prisma.month.create({
+        data: {
+          userId: ctx.session.user.id,
+          budget,
+          month,
+          year,
+        },
+      });
+      return { message: "Succesfully created budget for the month" };
     }),
 });
