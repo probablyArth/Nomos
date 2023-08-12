3d658ad7f87bafc85006f21119e80de826147f0b
= <=>
2023-08-12 11:40:20 +0530

    update prisma schema

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -13,12 +13,4 @@ export const exampleRouter = createTRPCRouter({
         greeting: `Hello ${input.text}`,
       };
     }),
-
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
-  }),
-
-  getSecretMessage: protectedProcedure.query(() => {
-    return "you can now see this secret message!";
-  }),
 });
